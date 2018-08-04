e9e70f1e59ae8543c0bf711ec007b577587d2fe8
Jonathan Ziller <jonathan.ziller@gmail.com>
2018-08-04 19:59:59 +0200

    build: update dependencies

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,6 +1,6 @@
-import winston, { Logger } from 'winston';
+import winston from 'winston';
 
-export const logger = new (Logger)({
+export const logger = winston.createLogger({
   transports: [
     new (winston.transports.Console)({ level: process.env.NODE_ENV === 'production' ? 'error' : 'debug' }),
     new (winston.transports.File)({ filename: 'debug.log', level: 'debug' }),
