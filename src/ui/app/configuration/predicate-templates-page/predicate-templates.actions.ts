add924d5eacdd0a70a92e805b1423ed9ce32246a
Jonathan Ziller <jonathan.ziller@gmail.com>
2018-08-05 18:10:54 +0200

    refactor: remove obsolete code

--- src/ui/app/configuration/predicate-templates-page/predicate-templates.actions.ts
+++ src/ui/app/configuration/predicate-templates-page/predicate-templates.actions.ts
@@ -16,13 +16,7 @@ export class InitializePredicateTemplatesPageAction implements Action {
   ) {}
 }
 
-export class OpenNewPredicateTemplateDialogAction implements Action {
-  static readonly TYPE = 'configuration/predicate-templates-page/OPEN_NEW_PREDICATE_TEMPLATE_DIALOG';
-  readonly type = OpenNewPredicateTemplateDialogAction.TYPE;
-}
-
 export type PredicateTemplatesPageActions =
   | LoadPredicateTemplatesPageDataAction
   | InitializePredicateTemplatesPageAction
-  | OpenNewPredicateTemplateDialogAction
   ;
