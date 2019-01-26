8efd4b5323df1da8a819aa5d6bc1b6d5066df865
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-01-26 12:02:19 +0100

    build: fix issues with UI prod build

--- src/ui-infrastructure/tsx.ts
+++ src/ui-infrastructure/tsx.ts
@@ -1,35 +1,33 @@
-import { ComponentOptions, CreateElement, FunctionalComponentOptions } from 'Vue';
+import { ComponentOptions, CreateElement, FunctionalComponentOptions, RenderContext } from 'Vue';
 import { Component } from 'vue-property-decorator';
 import { keys } from '../util';
 import { TsxComponent } from './tsx-component';
 
 export function pure<TProps>(def: (props: TProps) => JSX.Element) {
-  const name = def.name.replace(/Def$/g, '');
+  const name = (def.name || 'Component').replace(/Def$/g, '');
   return Component({ functional: true } as any as ComponentOptions<TsxComponent<TProps>>)(
     class extends TsxComponent<TProps> {
-      private eventHandlers: { [name: string]: Function } = {};
-
       // @ts-ignore
       static get name() {
         return name;
       }
 
-      created() {
-        keys(this.$listeners).forEach(name => {
+      render(h: CreateElement, context: RenderContext<TProps>) {
+        // babel plugin will transpile method to function component definition
+        const options = def as any as FunctionalComponentOptions<TProps>;
+
+        const eventHandlers: { [name: string]: Function } = {};
+        keys(context.listeners).forEach(name => {
           const normalizedName = `on${name.replace(/^on/g, '').replace(/^(.)/, v => v.toUpperCase())}`;
-          let handler = this.$listeners[name];
+          let handler = context.listeners[name];
           handler = Array.isArray(handler) ? () => {
             const args = arguments;
             (handler as Function[]).forEach(h => h(...args));
           } : handler;
-          this.eventHandlers[normalizedName] = handler;
+          eventHandlers[normalizedName] = handler;
         });
-      }
 
-      render(h: CreateElement) {
-        // babel plugin will transpile method to function component definition
-        const options = def as any as FunctionalComponentOptions<TProps>;
-        const props = { ...this.$attrs, ...this.eventHandlers } as any;
+        const props = { ...context.props, ...eventHandlers } as any;
         return options.render!.bind(undefined)(h, props);
       }
     }
